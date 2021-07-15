c4ff41a9923ba0550a3868f8ab92eae9cb75ffa3
Nica Cave <nicolacave@gmail.com>
2021-07-14 18:05:47 -0600

    functions

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,5 +1,14 @@
-class homeless:
-    def __init__(self, df) -> None:
-        self.df = df
+class homeless():
 
-    def rename(self, old_names, new_names):
+    def __init__(self, df, year):
+        self.df = df
+        self.year = year
+
+    def format_df(self):
+        self.df['year'] = self.year
+        self.df.replace(',','', regex=True, inplace=True)
+        self.df.loc[8] = self.df.loc[8].astype(float)
+        self.df.loc[4] = self.df.loc[4].astype(float)
+        self.df['unsheltered rate'] = (self.df.loc[8] / self.df.loc[4])
+        df = self.df.loc[:, ['year', 'CoC Number', 'CoC Name', 'unsheltered rate']]
+        return df 
